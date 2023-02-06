89db907074c58044fe7ffaffc97913f0334216f0
Brandon <brandonfuentes754@gmail.com>
2023-02-05 22:37:19 -0800

    fixed bugs on reset animation and power button

--- home_js.js
+++ home_js.js
@@ -1,14 +1,16 @@
 const outputDiv = document.getElementById('animated_p');
 const text = 'Line to be animated Line to be animated Line to be animated Line to be animated Line to be animated Line to be animated Line to be animated Line to be animated Line to be animated Line to be animated';
 const text_cursor = '|';
-let intervalID2;
-let reseted = false
+let intervalID_text;
+let intervalID_text_cursor;
+let reseted = false;
 
 reset(reseted);
 
 function reset(r){
     if(r){ //stops last animation
-        clearInterval(intervalID2)
+        clearInterval(intervalID_text_cursor);
+        clearInterval(intervalID_text);
     } 
 
     let text_cursor_on = false;
@@ -16,16 +18,16 @@ function reset(r){
     let i = 0;
     outputDiv.innerHTML = '';
 
-    const intervalID = setInterval(function(){
+    intervalID_text = setInterval(function(){
         outputDiv.innerHTML += text[i];
         i++;
         if(i === text.length){
-            clearInterval(intervalID);
+            clearInterval(intervalID_text);
             func_one_done = true;
         }
     },50);
     
-    intervalID2 = setInterval(function(){
+    intervalID_text_cursor = setInterval(function(){
         if(func_one_done){
             if(!text_cursor_on){
                 outputDiv.innerHTML += text_cursor;
@@ -40,14 +42,14 @@ function reset(r){
 }
 
 let color_rotation = ['blue','green','purple','orange','yellow','brown','white','black','red'];
-let color_interval = 0;
+var color_interval = 0;
+var color_var = document.querySelector(':root');
 
 function change_color(){
-    if(color_interval == length.color_rotation){
+    if(color_interval === (color_rotation.length)){
         color_interval = 0;
+        console.log(color_interval)
     }
-    var color_var = document.querySelector(':root');
     color_var.style.setProperty('--main_tittle_color', color_rotation[color_interval]);
     color_interval += 1;
-
 }
